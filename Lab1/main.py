3eed836430311ba38384549b27ae79ec10593018
wampir1408 <joanna.furmaniak@student.put.poznan.pl>
2016-10-16 13:29:36 +0200

    Final result

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,18 +1,35 @@
 from __future__ import division
 import numpy as np
-
+from matplotlib.backends.backend_pdf import PdfPages
+import matplotlib.pyplot as plt
 
 cols_count = 0
 row_count = 0
 non_zero = 0
 x_data = np.loadtxt(open("X_train.csv", "r"), delimiter=",")
 for row in x_data:
+    print("Waiting")
     row_count += 1
     if (cols_count < len(row)):
         cols_count = len(row)
     for column in row:
         if (column != 0):
             non_zero += 1
+
+file = open("results.txt", "w")
+index = 0
+with PdfPages('histograms.pdf') as pdf:
+    for column in x_data.T:
+        file.write("Cecha nr " + str(index) + "\n")
+        file.write("Srednia: " + str(sum(column) / float(len(column))) + "\n")
+        file.write("Wariancja: " + str(np.var(column)) + "\n")
+        file.write("Unikalne wartosci: " + str(len(set(column))) + "\n")
+        plt.hist(column)
+        plt.title("Cecha nr " + str(index))
+        pdf.savefig()
+        plt.close()
+        index += 1
+
 
 array = np.genfromtxt('Y_train.csv', delimiter="\n")
 unique_set = set(array)
@@ -26,9 +43,13 @@ for value in array:
 
 dom_class = max(classes, key=classes.get)
 
-print("Dominujaca klasa oraz jej prawdopodobienstwo: " + str(dom_class) + " " + str(classes[dom_class]/len(array)))
+file.write("Dominujaca klasa oraz jej prawdopodobienstwo: " + str(dom_class) + " " + str(classes[dom_class]/len(array)) + "\n")
 
 
-print("Liczba cech: " + str(cols_count))
-print("Liczba przykladow: " + str(row_count))
-print("Liczba niezerowych wartosci cech: " + str(non_zero))
+file.write("Liczba cech: " + str(cols_count) + "\n")
+file.write("Liczba przykladow: " + str(row_count) + "\n")
+file.write("Liczba niezerowych wartosci cech: " + str(non_zero) + "\n")
+
+file.close()
+
+print("Done")
